1e2d7019e936b25507ccbf8be484cb0c65e58780
Dennis <jokarboxing@gmail.com>
2019-04-13 23:53:00 +0200

    0.0.4

--- Scripts/test.cs
+++ Scripts/test.cs
@@ -10,6 +10,9 @@ public const float maxVel = 0.1f;
 private Rigidbody2D rb2d;
 private Animator anim;
 	public bool playerAlive = true;
+	public bool attack = false;
+		private float attackMaxIterationTime = 10; //attack animation time
+		private float attackIterationTime = 0;
 	
 	void Start(){
 		rb2d = GetComponent<Rigidbody2D>();
@@ -21,25 +24,30 @@ private Animator anim;
 	private bool idle = true;
 
 	void Update(){
-			anim.SetBool("MovX", movX);
-			anim.SetBool("MovY", movY);
-			anim.SetBool("idle", idle);
-			anim.SetBool("alive", playerAlive);
+		anim.SetBool("MovX", movX);
+		anim.SetBool("MovY", movY);
+		anim.SetBool("idle", idle);
+		anim.SetBool("alive", playerAlive);
+		anim.SetBool("attack", attack);
 
-		if(playerAlive == false){
-			reloadScene();
-		}
+			if(playerAlive == false){
+				reloadScene();
+			}
+			if(Input.GetMouseButtonDown(0)){
+				attack = true;
+				attackIterationTime = attackMaxIterationTime;
+			}
+				idle = true;
+				movX = false;
+				movY = false;
 	}
 	void FixedUpdate(){
 		move();
+		calcAttackTime();
 	}
 
 
 		void move(){
-				idle = true;
-				movX = false;
-				movY = false;
-			
 			if(Input.GetKey(KeyCode.W)){
 			rb2d.position += new Vector2(0,maxVel);
 				movY = true;
@@ -61,6 +69,18 @@ private Animator anim;
 				idle = false;
 			}
 		}
+
+			void calcAttackTime(){
+				if(attack){
+					if(attackIterationTime == 0){
+						attack = false;
+					}
+					else{
+						attackIterationTime--;
+					}
+				}
+			
+			}
 
 			void calcAlive(){
 				if(playerAlive != true){
